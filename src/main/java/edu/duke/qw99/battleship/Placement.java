244447dc4b25f5db0fc7cb47267548fc814e61fd
Qiutong Wang <qiutong.wang@duke.edu>
2021-02-01 19:23:30 +0800

    placement

---
+++ src/main/java/edu/duke/qw99/battleship/Placement.java
@@ -0,0 +1,57 @@
+package edu.duke.qw99.battleship;
+
+public class Placement {
+  private final Coordinate where;
+  private final char orientation;
+
+  public Placement(Coordinate cdnt, char c) {
+    this.where = cdnt;
+    this.orientation = Character.toUpperCase(c);
+  }
+
+  public Placement(String s){
+    if(s.length() < 3){
+      throw new IllegalArgumentException("Invalid placement");
+    }
+    String str_cdnt = s.substring(0, s.length() - 1);
+    Coordinate cdnt = new Coordinate(str_cdnt);
+    this.where = cdnt;
+    if(Character.toUpperCase(s.charAt(s.length() - 1)) < 'A' || Character.toUpperCase(s.charAt(s.length() - 1)) > 'Z'){
+      throw new IllegalArgumentException("Invalid orientation");
+    }
+    this.orientation = s.charAt(s.length() - 1);
+  }
+    
+  public Coordinate getWhere() {
+    return where;
+  }
+
+  public char getOrientation() {
+    return orientation;
+  }
+
+  @Override
+  public boolean equals(Object o){
+    if(o.getClass().equals(getClass())){
+      Placement p = (Placement) o;
+      return where == p.where && orientation == p.orientation;
+    }
+    return false;
+  }
+
+  @Override
+  public String toString(){
+    StringBuilder ans = new StringBuilder();
+    ans.append((char)('A' + where.getRow()));
+    ans.append(Integer.toString(where.getColumn()));
+    ans.append(orientation);
+    return ans.toString();
+  }
+
+  @Override
+  public int hashCode(){
+    return toString().hashCode();
+  }
+}
+
+
---
+++ src/test/java/edu/duke/qw99/battleship/PlacementTest.java
@@ -0,0 +1,76 @@
+package edu.duke.qw99.battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PlacementTest {
+  @Test
+  public void test_cdnt_and_orientation() {
+    Coordinate c1 = new Coordinate(10, 20);
+    Placement p1 = new Placement(c1, 'v');
+    assertEquals(c1, p1.getWhere());
+    assertEquals('V', p1.getOrientation());
+  }
+
+  @Test
+  public void test_equals() {
+    Coordinate c1 = new Coordinate(1, 2);
+    Coordinate c2 = new Coordinate(1, 3);
+    Placement p1 = new Placement(c1, 'v');
+    Placement p2 = new Placement(c1, 'V');
+    Placement p3 = new Placement(c2, 'v');
+    Placement p4 = new Placement(c2, 'a');
+    assertEquals(p1, p1);
+    assertEquals(p1, p2);
+    assertNotEquals(p1, p3);
+    assertNotEquals(p1, p4);
+    assertNotEquals(p1, c1);
+  }
+
+  @Test
+  public void test_toString(){
+     Coordinate c1 = new Coordinate(1, 2);
+     Placement p1 = new Placement(c1, 'v');
+     assertEquals("B2V", p1.toString());
+  }
+
+  @Test
+  public void test_hashCode() {
+    Coordinate c1 = new Coordinate(1, 2);
+    Coordinate c2 = new Coordinate(1, 3);
+    Placement p1 = new Placement(c1, 'v');
+    Placement p2 = new Placement(c1, 'V');
+    Placement p3 = new Placement(c1, 'a');
+    Placement p4 = new Placement(c2, 'v');
+    assertEquals(p1.hashCode(), p2.hashCode());
+    assertNotEquals(p1.hashCode(), p4.hashCode());
+    assertNotEquals(p1.hashCode(), p3.hashCode());
+  }
+
+  @Test
+  public void test_string_constructor_valid_cases(){
+    Placement p1 = new Placement("D4v");
+    assertEquals(new Coordinate("D4"), p1.getWhere());
+    assertEquals('v', p1.getOrientation());
+  }
+
+  @Test
+  public void test_string_constructor_error_cases(){
+    assertThrows(IllegalArgumentException.class, () -> new Placement("a3n"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("A"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("@0"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("A12"));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
